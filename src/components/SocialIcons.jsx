aec8665283eb5053424242b1ce7c42c85fee95b2
me1sh <442103118@student.ksu.edu.sa>
2023-09-06 13:19:05 +0300

    ScoialIcons

---
+++ src/components/SocialIcons.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { createPortal } from 'react-dom';
+import { IconContext } from "react-icons";
+import { FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import "./SocialIcons.css";
+
+function SocialIcons() {
+    
+  return createPortal(
+    
+    <IconContext.Provider
+      value={{
+        size: "2rem",
+        color: "white",
+        className: "mx-auto my-3",
+      }}
+    >
+      <div className="w-10 flex flex-col shadow-2xl self-end mt-3 mr-3 rounded-lg socialIcons hidden lg:block">
+        <a href="https://twitter.com/ftcksu" target="_blank">
+          <FaTwitter />
+        </a>
+        <a href="https://www.instagram.com/ftcksu/" target="_blank">
+          <FaInstagram />
+        </a>
+
+        <a href="https://www.youtube.com/@FTCKSU" target="_blank">
+          <FaYoutube />
+        </a>
+        <a href="https://www.instagram.com/ftcksu/" target="_blank">
+          <AiOutlineMail />
+        </a>
+      </div>
+    </IconContext.Provider>
+  ,document.body);
+}
+
+export default SocialIcons;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
-
 import Pannel from "../components/Pannel";
 import SocialMedia from "../components/SocialMedia";
 import Activites from "../components/Activities";
+import SocialIcons from "../components/SocialIcons";
 
 function Home() {
-    return(
-        <div className="flex flex-col justify-start gap-y-10 h-screen overflow-x-hidden animate-flip-down animate-once">
-            <div >
-                <div className="w-full text-center text-3xl my-4" >معرفة •  تقنية • ابداع</div>
-            </div>
-            <Pannel/>
-            <SocialMedia/>
-            <Activites/>
+  return (
+    <div className="flex flex-col justify-start gap-y-10 h-screen overflow-x-hidden animate-flip-down animate-once">
+      <SocialIcons />
+      <div>
+        <div className="w-full text-center text-3xl my-4">
+          معرفة • تقنية • ابداع
         </div>
-    )
+      </div>
+      <Pannel />
+      <SocialMedia />
+      <Activites />
+    </div>
+  );
 }
 
-
-
-export default Home
+export default Home;
